afcc45bd6ca9751984146a987d86854b75c10ba1
javorszky <javorszky@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-27 23:21:23 +0000

    missed changing the version
    git-svn-id: http://plugins.svn.wordpress.org/ghost/trunk@974357 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ghost.php
+++ ghost.php
@@ -15,7 +15,7 @@
  * Plugin Name: Ghost
  * Plugin URI:  http://ghost.org
  * Description: Plugin to export your WordPress blog so you can import it into your Ghost installation
- * Version:     0.4.3
+ * Version:     0.5.0
  * Author:      Ghost Foundation
  * Author URI:  http://ghost.org
  * Text Domain: wp2ghost

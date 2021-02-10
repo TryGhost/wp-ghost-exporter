99b834ad260850ecfca8c68c77e9a3906cb15a12
Paul Davis <pauladamdavis@gmail.com>
2021-02-10 11:45:32 +0000

    Clean up UI
    Refs #18
    
    - Removes calls for Open Sans webfont
    - Removes many custom styles, opting to use WP defaults
    - Fixes Ghost icon display
    - Updates screenshot

--- views/admin.php
+++ views/admin.php
@@ -13,16 +13,22 @@
  */
 ?>
 <div class="wrap" id="ghost">
-	<div id="icon-ghost" class="icon32"><br></div>
+	<div id="icon-ghost"></div>
+	
 	<h2 class="title"><?php echo esc_html( get_admin_page_title() ); ?></h2>
+	
 	<p>Hey there! We’re excited to help you migrate WordPress content over to Ghost, and this plugin is designed to help with that process by exporting your WP content into a set of files which Ghost should be able to import cleanly.</p>
+	
 	<h3>Things to keep in mind</h3>
+	
 	<ol>
 		<li>Tags will be migrated, but not categories. If needed, you can <a href="https://wordpress.org/plugins/wpcat2tag-importer/" target="_blank">convert your categories to tags</a> before exporting.</li>
-		<li>Ghost does not have built-in comments, but it does integrate with <a href="https://ghost.org/integrations/community/" target="_blank">many comment platforms</a> if you want to migrate your comments there.</li>
+		<li>Ghost does not have built-in comments, but it does integrate with <a href="https://ghost.org/integrations/?tag=community" target="_blank">many comment platforms</a> if you want to migrate your comments there.</li>
 		<li>No custom fields, meta, shortcodes, post types, taxonomies or binary files will be migrated. Just regular <strong>posts</strong>, <strong>pages</strong>, <strong>tags</strong> and <strong>images</strong>.</li>
 	</ol>
+	
 	<h3>Steps to follow</h3>
+	
 	<ol>
 		<li>Click the "Download Ghost file" button. You will receive an import file for Ghost.</li>
 		<li>Log into your Ghost site, and head to the “Labs” section in admin and import the file.</li>
@@ -34,24 +40,24 @@
 		<?php submit_button( __( 'Download Ghost File' ) ); ?>
 	</form>
 
-	<p>Struggling with the zip file? Download the <a href="<?php get_admin_url();?>tools.php?ghostjsonexport=true" target="_blank"><code>.json</code></a> instead.</p>
+	<p>Struggling with the zip file? Download the <code>.json</code> instead.</p>
+	
+	<p><a class="button-secondary" href="<?php get_admin_url();?>tools.php?ghostjsonexport=true" target="_blank">Download JSON</a></p>
+
 	<hr/>
+
 	<div id="ghost-diagnostics">
-		<h4>Ghost Migrator <?php
-		// set diagnostic variables
-	  $gmaxexecutiontime = ini_get('max_execution_time');
-	  $gmemorylimit = ini_get('memory_limit');
-		$ghostmigrator = new ghost();
-		echo $ghostmigrator->getghostmigratorversion();
-		?> - Diagnostics</h4>
+		<?php
+			// Set diagnostic variables
+			$gMaxExecutionTime = ini_get('max_execution_time');
+			$gMemoryLimit = ini_get('memory_limit');
+			$ghostMigrator = new ghost();
+			$zipArchiveInstalled = (class_exists('ZipArchive')) ? 'Yes' : 'No';
+		?>
+		<h4>Ghost Migrator <?php echo $ghostMigrator->getghostmigratorversion(); ?> - Diagnostics</h4>
 		<p>PHP version: <?php echo phpversion();?>.</p>
-		<p>PHP ZipArchive Installed: <?php if (class_exists('ZipArchive')) {
-	    	echo "Yes";
-	  	} else {
-	  		echo "No";
-	  	} ?>
-		</p>
-		<p>Memory Limit: <?php echo $gmemorylimit;?></p>
-		<p>Max Execution Time: <?php echo $gmaxexecutiontime;?></p>
+		<p>PHP ZipArchive Installed: <?php echo $zipArchiveInstalled; ?></p>
+		<p>Memory Limit: <?php echo $gMemoryLimit;?></p>
+		<p>Max Execution Time: <?php echo $gMaxExecutionTime;?></p>
 	</div>
 </div>
